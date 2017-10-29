98406ede8e1461b1ec9c565e0e77e85fb99c1ff2
mseibt <michael.seibt@tum.de>
2017-10-29 19:01:26 +0100

    added config loading path as argument

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
+
+from ui import ui
 from state import BrightnessState
 
 
 if __name__ == "__main__":
-    brightness_state = BrightnessState()
+    if len(sys.argv) > 1:
+        brightness_state = BrightnessState(sys.argv[1])
+    else:
+        brightness_state = BrightnessState()
     ui(brightness_state)
